23c889fe28088b0f85d4bcc9bd5277827bc2c20b
kwame <yamoahkwame@gmail.com>
2020-11-23 07:12:15 +0000

    Deleting old version of Paint Application

--- Repos/Java/VectorVisualization/src/Exceptions/IncorrectInputException.java
+++
@@ -1,9 +0,0 @@
-package Exceptions;
-
-public class IncorrectInputException extends Exception {
-    public IncorrectInputException(String error) {
-        super(error);
-    }
-
-
-}
--- Repos/Java/VectorVisualization/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module VectorVisualization {
-    requires javafx.fxml;
-    requires javafx.controls;
-    requires java.desktop;
-    requires junit;
-    opens sample;
-    opens Exceptions;
-}
